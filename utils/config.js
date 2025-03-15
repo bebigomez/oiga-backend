895d316db3e2c6e55ef437ebc4c74c5cce547e94
Victor Gomez <vhgomez.py@gmail.com>
2025-03-15 20:06:57 -0300

    use Trello API for orders

--- utils/config.js
+++ utils/config.js
@@ -4,23 +4,33 @@ const knex = require('knex')
 
 const PORT = process.env.PORT
 
-const DB_CONNECTION = process.env.NODE_ENV === 'test'
-  ? {
-    host: process.env.TEST_DB_HOST,
-    user: process.env.TEST_DB_USER,
-    password: process.env.TEST_DB_PASSWORD,
-    database: process.env.TEST_DB_NAME,
-    port: process.env.TEST_DB_PORT,
-  }
-  : {
+const DB_CONFIGS = {
+  production: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: 5432,
-    ssl: { rejectUnauthorized: false }
-  }
+    ssl: { rejectUnauthorized: false },
+  },
+  development: {
+    host: process.env.DEV_DB_HOST,
+    user: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    port: process.env.DEV_DB_PORT,
+  },
+  test: {
+    host: process.env.TEST_DB_HOST,
+    user: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    port: process.env.TEST_DB_PORT,
+  },
+}
 
+const ENV = process.env.NODE_ENV || 'development'
+const DB_CONNECTION = DB_CONFIGS[ENV]
 
 const knexConfig = {
   client: 'pg',
@@ -30,12 +40,8 @@ const knexConfig = {
 const db = knex(knexConfig)
 
 db.raw('SELECT 1+1 AS result')
-  .then(() => {
-    console.log('Conexión a base de datos exitosa')
-  })
-  .catch((err) => {
-    console.error('Error de conexión:', err)
-  })
+.then(() => console.log(`Conexión exitosa a la BD (${ENV})`))
+.catch((err) => console.error('Error de conexión:', err))
 
 module.exports = {
   PORT,
